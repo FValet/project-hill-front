4be2f35bf6bc3e99cf3edd2b6465b73821277cde
Franck Valet <fvalet.dev@gmail.com>
2022-07-30 11:06:57 +0200

    feat: install @nuxtjs/tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: ['@nuxtjs/strapi'],
+    buildModules: ['@nuxtjs/strapi', '@nuxtjs/tailwindcss'],
 
     strapi: {
         url: process.env.STRAPI_URL || 'http://localhost:1337',
