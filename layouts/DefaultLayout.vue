c9ff0fb4ceca44e7feb59e0688c59a4e961ad771
Franck Valet <fvalet.dev@gmail.com>
2022-08-05 18:41:13 +0200

    feat: add default layout

---
+++ layouts/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen p-4 text-gray-200 bg-gray-800 max-w-screen">
+    <Nuxt class="mx-auto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
