e3b3dc087428d46afb4305b3996c276a8ef36a3f
Franck Valet <fvalet.dev@gmail.com>
2022-08-07 11:35:30 +0200

    feat: improve default layout with hero data fetch

--- layouts/DefaultLayout.vue
+++ layouts/DefaultLayout.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="min-h-screen p-4 text-gray-200 bg-gray-800 max-w-screen">
-    <Nuxt class="mx-auto" />
+  <div
+    v-if="loaded"
+    class="min-h-screen text-gray-200 bg-gray-800 max-w-screen relative"
+  >
+    <the-header class="sticky z-10 top-0" />
+    <div class="py-8 h-full">
+      <Nuxt class="mx-auto p-4" />
+    </div>
+    <the-footer class="sticky z-10 bottom-0" />
   </div>
 </template>
 
 <script>
+import { onMounted, ref } from '@nuxtjs/composition-api'
+import { useHeroStore } from '~/store/hero'
+
 export default {
   name: 'DefaultLayout',
+
+  setup() {
+    const loaded = ref(false)
+    const heroStore = useHeroStore()
+
+    onMounted(async () => {
+      await heroStore.getHero()
+      loaded.value = true
+    })
+
+    return { loaded }
+  },
 }
 </script>
