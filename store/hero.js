619b8ca308a02468f21e4b063f523752e3c6b4ec
Franck Valet <fvalet.dev@gmail.com>
2022-08-07 11:34:51 +0200

    feat: add hero store

---
+++ store/hero.js
@@ -0,0 +1,31 @@
+import { defineStore } from 'pinia'
+
+export const useHeroStore = defineStore({
+    id: 'hero',
+
+    state: () => ({
+        hero: {},
+        inventory: {},
+        level: {}
+    }),
+
+    actions: {
+        async getHero() {
+            try {
+                if (!this.hero.id) {
+                    const { data } = await this.$nuxt.$strapi.find('hero', { populate: '*' })
+                    this.hero = data
+                    this.inventory = data.attributes.inventory.data
+                    if (!this.level.id) {
+                        // , { 'levels.level': this.hero.attributes.level }
+                        const { data } = await this.$nuxt.$strapi.find('levels-table', { 'levels.level': this.hero.attributes.level, populate: '*' })
+                        // TODO: move level recuperation in a custom route to get level and other data
+                        this.level = data.attributes.levels.find(x => x.level === this.hero.attributes.level)
+                    }
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+})
